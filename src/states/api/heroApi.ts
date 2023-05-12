b297fe9aa2a2a0533a16e4a01cd68f59d215aff4
markshawn2020 <shawninjuly@gmail.com>
2023-05-12 19:33:29 +0800

    added init script

--- src/states/api/heroApi.ts
+++ src/states/api/heroApi.ts
@@ -2,23 +2,40 @@ import { baseApi } from '~/states/api/baseApi'
 import { IHero } from '~/ds/hero'
 import { IGraphData } from '~/ds/panel_3dgraph'
 
+export const TAG_HERO = 'hero' as const
 
-export const heroApi = baseApi.injectEndpoints({
-	overrideExisting: true,
-	endpoints: (build) => ({
-		getGraphData: build.query<IGraphData, void>({
-			query: () => `/hero/graph_data`,
+export const heroApi = baseApi
+	.enhanceEndpoints({
+		addTagTypes: [TAG_HERO],
+	})
+	.injectEndpoints({
+		overrideExisting: true,
+		endpoints: (build) => ({
+			
+			listHeroes: build.query<IHero[], void>({
+				query: () => `/hero/`,
+				providesTags: [TAG_HERO],
+			}),
+			
+			getGraphData: build.query<IGraphData, void>({
+				query: () => `/hero/graph_data`,
+				providesTags: [TAG_HERO],
+			}),
+			
+			initHeroes: build.mutation<void, void>({
+				query: () => ({
+					url: `/hero/init_from_notion`,
+					method: 'POST',
+				}),
+				invalidatesTags: [TAG_HERO],
+			}),
+			
 		}),
-		
-		listHeroes: build.query<IHero[], void>({
-			query: () => `/hero/`,
-		}),
-		
-	}),
-})
+	})
 
 
 export const {
+	useInitHeroesMutation,
 	useGetGraphDataQuery,
 	useListHeroesQuery,
 } = heroApi
