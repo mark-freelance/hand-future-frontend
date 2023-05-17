236f6bdd0bcc74e2a5c22417c4cdba9f31695d2b
markshawn2020 <shawninjuly@gmail.com>
2023-05-17 17:01:53 +0800

    added image uri track

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,8 +1,11 @@
 import { BACKEND_ENDPOINT } from '~/lib/env'
 
-export const normalizeImageUri = (imageId: string | null | undefined): string | undefined =>
-	!imageId
+export const normalizeImageUri = (imageId: string | null | undefined): string | undefined => {
+	const targetImgPath = !imageId
 		? undefined
 		: imageId.startsWith('/')
 			? BACKEND_ENDPOINT + imageId
 			: imageId
+	console.log({ imageId, targetImgPath })
+	return targetImgPath
+}
