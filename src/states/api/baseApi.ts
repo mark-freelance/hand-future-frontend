f213dd0352348041a1ec416aa565bed1506dd343
markshawn2020 <shawninjuly@gmail.com>
2023-05-17 16:33:00 +0800

    added backend

--- src/states/api/baseApi.ts
+++ src/states/api/baseApi.ts
@@ -3,11 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 import { BACKEND_ENDPOINT } from '~/lib/env'
 
+console.log({ BACKEND_ENDPOINT })
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
 	reducerPath: 'baseApi',
-	baseQuery: fetchBaseQuery({ baseUrl: BACKEND_ENDPOINT }),
+	baseQuery: fetchBaseQuery({
+		baseUrl: BACKEND_ENDPOINT,
+	}),
 	endpoints: (builder) => ({}),
 })
 
