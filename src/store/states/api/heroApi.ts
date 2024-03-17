4989894bfd8d8dc428d5d02c1f5c1ad4da836f1f
markshawn2020 <shawninjuly@gmail.com>
2024-03-18 01:22:36 +0800

    dropped old useInitHeroesMutation

--- src/store/states/api/heroApi.ts
+++ src/store/states/api/heroApi.ts
@@ -1,41 +1,41 @@
-import { baseApi } from './baseApi'
-import { IHero } from '../../../schema/hero'
-import { IGraphData } from '../../../schema/panel_3dgraph'
+import { trpc } from "../../../lib/trpc";
+import { IHero } from "../../../schema/hero";
+import { IGraphData } from "../../../schema/panel_3dgraph";
+import { baseApi } from "./baseApi";
 
-export const TAG_HERO = 'hero' as const
+export const TAG_HERO = "hero" as const;
 
 export const heroApi = baseApi
-	.enhanceEndpoints({
-		addTagTypes: [TAG_HERO],
-	})
-	.injectEndpoints({
-		overrideExisting: true,
-		endpoints: (build) => ({
-			
-			listHeroes: build.query<IHero[], void>({
-				query: () => `/hero/`,
-				providesTags: [TAG_HERO],
-			}),
-			
-			getGraphData: build.query<IGraphData, void>({
-				query: () => `/hero/graph_data`,
-				providesTags: [TAG_HERO],
-			}),
-			
-			initHeroes: build.mutation<void, void>({
-				query: () => ({
-					url: `/hero/init_from_notion`,
-					method: 'POST',
-				}),
-				invalidatesTags: [TAG_HERO],
-			}),
-			
-		}),
-	})
+  .enhanceEndpoints({
+    addTagTypes: [TAG_HERO],
+  })
+  .injectEndpoints({
+    overrideExisting: true,
+    endpoints: (build) => ({
+      listHeroes: build.query<IHero[], void>({
+        query: () => `/hero/`,
+        providesTags: [TAG_HERO],
+      }),
 
+      getGraphData: build.query<IGraphData, void>({
+        query: () => `/hero/graph_data`,
+        providesTags: [TAG_HERO],
+      }),
+
+      initHeroes: build.mutation<void, void>({
+        query: () => ({
+          url: `/hero/init_from_notion`,
+          method: "POST",
+        }),
+        invalidatesTags: [TAG_HERO],
+      }),
+    }),
+  });
 
 export const {
-	useInitHeroesMutation,
-	useGetGraphDataQuery,
-	useListHeroesQuery,
-} = heroApi
+  // useInitHeroesMutation,
+  useGetGraphDataQuery,
+  useListHeroesQuery,
+} = heroApi;
+
+export const useInitHeroesMutation = trpc.graph.init.useMutation;
