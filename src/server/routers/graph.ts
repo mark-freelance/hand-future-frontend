c83dfb135751fb5da7518ceebd7268cd0fde3b66
markshawn2020 <shawninjuly@gmail.com>
2024-03-17 15:16:12 +0800

    added env

--- src/server/routers/graph.ts
+++ src/server/routers/graph.ts
@@ -1,4 +1,11 @@
+import { Client } from "@notionhq/client";
+import { env } from "~/env";
 import { procedure, router } from "../trpc";
+
+// Initializing a client
+const notion = new Client({
+  auth: env.NOTION_TOKEN,
+});
 
 export const graphRouter = router({
   init: procedure.query(async ({}) => {
