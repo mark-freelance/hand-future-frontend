9c5d26e359c49b66d06177b62b145c6103a8b07e
markshawn2020 <shawninjuly@gmail.com>
2024-03-17 15:37:34 +0800

    updated actions

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,4 +1,6 @@
 import { Client, collectPaginatedAPI } from "@notionhq/client";
+import { promises } from "fs";
+import * as path from "path";
 import { env } from "../env";
 
 const notion = new Client({
@@ -11,7 +13,11 @@ export const readNotion = async () => {
   const data = await collectPaginatedAPI(notion.databases.query, {
     database_id: env.NOTION_DATABASE_ID,
   });
-  console.log("-- inited data: ", data);
+
+  const fp = path.join(__dirname, "data.json");
+  console.log(`-- dumping data info ${fp}`);
+  await promises.writeFile(fp, JSON.stringify(data, null, 2));
+  console.log("-- dumped");
 };
 
 void readNotion();
