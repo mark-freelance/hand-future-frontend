a0183c4fd47a4408cfb9df05b40b7950197aff4f
markshawn2020 <shawninjuly@gmail.com>
2024-11-18 00:13:32 +0800

    [playground] basic gui

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,44 +4,44 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import axios from "axios"
+import type { AxiosRequestConfig } from "axios"
+import _ from "lodash"
 
-import _ from 'lodash'
-import axios from 'axios'
+import { BACKEND_ENDPOINT } from "./env"
 
-import { BACKEND_ENDPOINT } from '~/lib/env'
+axios.defaults.withCredentials = true
 
-import type { AxiosRequestConfig } from 'axios'
+export const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 
-export const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-
-export const TOKEN_KEY = 'Authorization'
-
+export const TOKEN_KEY = "Authorization"
 
 export const AXIOS_INIT_CONFIG: AxiosRequestConfig = {
-	baseURL: BACKEND_ENDPOINT,
-	timeout: 10000, // ref: https://stackoverflow.com/a/41754655/9422455
-	headers: {
-		'User-Agent': USER_AGENT,
-	},
-	withCredentials: true,
+  baseURL: BACKEND_ENDPOINT,
+  timeout: 10000, // ref: https://stackoverflow.com/a/41754655/9422455
+  headers: {
+    "User-Agent": USER_AGENT,
+  },
+  withCredentials: true,
 }
-
 
 export const backendAPI = axios.create(AXIOS_INIT_CONFIG)
 
-
 backendAPI.interceptors.request.use(reqConfig => {
-	const basicConfig = _.cloneDeep(AXIOS_INIT_CONFIG)
-	reqConfig = _.merge( // inspired by https://stackoverflow.com/a/58789480/9422455
-		basicConfig,
-		reqConfig,
-	)
-	return reqConfig
+  const basicConfig = _.cloneDeep(AXIOS_INIT_CONFIG)
+  reqConfig = _.merge(
+    // inspired by https://stackoverflow.com/a/58789480/9422455
+    basicConfig,
+    reqConfig,
+  )
+  return reqConfig
 })
 
-backendAPI.interceptors.response.use(response =>
-	// console.debug('res:', response.request.url)
-	response,
+backendAPI.interceptors.response.use(
+  response =>
+    // console.debug('res:', response.request.url)
+    response,
 )
 
 export default backendAPI
