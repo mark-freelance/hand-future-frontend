bf3250501737c49f7a1d9ade39caa062db1952d3
markshawn2020 <shawninjuly@gmail.com>
2024-11-19 06:50:05 +0800

    drop mongo

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,6 +5,3 @@ console.log({ BACKEND_ENDPOINT })
 
 export const EMAIL_SERVER = process.env.EMAIL_SERVER
 export const EMAIL_FROM = process.env.EMAIL_FROM
-
-export const DATABASE_AUTH_DB_NAME = process.env.DATABASE_AUTH_DB_NAME
-export const DATABASE_MONGODB_URI = process.env.DATABASE_MONGODB_URI
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,57 +1,45 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import EmailProvider from 'next-auth/providers/email'
-import { createTransport } from 'nodemailer'
+import NextAuth, { type NextAuthOptions } from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import { createTransport } from "nodemailer"
+import { generateVerificationToken } from "~/lib/auth"
 
-import { DATABASE_AUTH_DB_NAME, EMAIL_FROM, EMAIL_SERVER } from '@/lib/env'
-import clientPromise from '@/lib/mongodb'
-import { setTokenCentre } from '@/pages/api/auth/general'
-import { generateVerificationToken } from '~/lib/auth'
-import { MongodbEmailAdapter } from '~/lib/mongodb-email-adapter'
-
+import { EMAIL_FROM, EMAIL_SERVER } from "@/lib/env"
+import { setTokenCentre } from "@/pages/api/auth/general"
 
 export const authOptions: NextAuthOptions = {
-	adapter: MongodbEmailAdapter(clientPromise, {
-		databaseName: DATABASE_AUTH_DB_NAME,
-		collections: { Users: 'user' },
-	}),
-	
-	providers: [
-		EmailProvider({
-			
-			server: EMAIL_SERVER,
-			from: EMAIL_FROM,
-			// maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-			generateVerificationToken,
-			
-			async sendVerificationRequest(params) {
-				const { identifier, url, provider, theme, token } = params
-				const transport = createTransport(provider.server)
-				const result = await transport.sendMail({
-					to: identifier,
-					from: provider.from,
-					subject: `Your Activation Code：${token}`,
-					// text: text({ url, host }),
-					// html: html({ url, host, theme }),
-				})
-				setTokenCentre(identifier, token)
-				const failed = result.rejected.concat(result.pending).filter(Boolean)
-				if (failed.length) throw new Error(`Email (${failed.join(', ')}) could not be sent`)
-			},
-			
-		}),
-	],
-	
-	callbacks: {
-		session({ session, token, user }) {
-			// console.log('session', { session, token, user })
-			session.user.id = user.id
-			return session // The return type will match the one returned in `useSession()`
-		},
-	},
-	
-	pages: {
-		signIn: '/auth/signin',
-	},
+  providers: [
+    EmailProvider({
+      server: EMAIL_SERVER,
+      from: EMAIL_FROM, // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+      generateVerificationToken,
+
+      async sendVerificationRequest(params) {
+        const { identifier, url, provider, theme, token } = params
+        const transport = createTransport(provider.server)
+        const result = await transport.sendMail({
+          to: identifier,
+          from: provider.from,
+          subject: `Your Activation Code：${token}`, // text: text({ url, host }),
+          // html: html({ url, host, theme }),
+        })
+        setTokenCentre(identifier, token)
+        const failed = result.rejected.concat(result.pending).filter(Boolean)
+        if (failed.length) throw new Error(`Email (${failed.join(", ")}) could not be sent`)
+      },
+    }),
+  ],
+
+  callbacks: {
+    session({ session, token, user }) {
+      // console.log('session', { session, token, user })
+      session.user.id = user.id
+      return session // The return type will match the one returned in `useSession()`
+    },
+  },
+
+  pages: {
+    signIn: "/auth/signin",
+  },
 }
 
 export default NextAuth(authOptions)
